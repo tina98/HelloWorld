a1805459bdffb410ba09c1cfcaf8923625bacae1
tina98 <tinali3698@gmail.com>
2014-12-11 01:35:29 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 $(document).ready(function(){
 
     $(window).scroll(function () {
-      if ($(window).scrollTop()>=80){
-          $('p').html($(window).height());
-          $('h1').html($(window).scrollTop());
+      if ($(window).scrollTop()>=($(window).height()-$("#navbar").height())){
+        //   $('p').html($(window).height());
+        //   $('h1').html($(window).scrollTop());
         $('#navbar').css("top","0vw")
           .css("position","fixed")
           .css("background-color","rgba(20, 23, 23, 0.7)");
