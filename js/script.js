072994432096407a783ab53ff53040a32b84c2b5
tina98 <tinali3698@gmail.com>
2014-12-11 01:26:37 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 
     $(window).scroll(function () {
-      if ($(window).scrollTop()>=$(window).height()-$("#navbar").height()){
+      if ($(window).scrollTop()>=$(window).height()-$("#navbar").height()*2){
         $('#navbar').css("top","0vw")
           .css("position","fixed")
           .css("background-color","rgba(20, 23, 23, 0.7)");
