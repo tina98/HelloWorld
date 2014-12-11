f328c8628ac0546c5208fca568df8972c47d2222
tina98 <tinali3698@gmail.com>
2014-12-11 01:21:19 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,20 +1,17 @@
 $(document).ready(function(){
   if($(window).width() >= 1000){
-        $(window).scroll(function () {
+    $(window).scroll(function () {
             
-            if ($(window).scrollTop()>=80){
-                $('#navbar').css("top","0vw")
-                        .css("position","fixed")
-                        .css("background-color","rgba(20, 23, 23, 0.7)");
+      if ($(window).scrollTop()>=80){
+        $('#navbar').css("top","0vw")
+          .css("position","fixed")
+          .css("background-color","rgba(20, 23, 23, 0.7)");
             
-            } else {
-                $('#navbar')/*.css("top","7vw")*/
-                        .css("position","static")
-                        .css("background-color","rgba(20, 23, 23, 1)");
-
-               
-            }  
-        });
-    }
-
-}
+      } else {
+        $('#navbar')/*.css("top","7vw")*/
+          .css("position","static")
+          .css("background-color","rgba(20, 23, 23, 1)");
+      }  
+    });
+  }
+})
