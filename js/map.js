74bdbef663423edcea31a329b8c4219e27db5f51
tina98 <tinali3698@gmail.com>
2014-12-13 00:38:20 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,8 +1,17 @@
-function initialize(){
-  var mapO={
-    center: {lat: 42.613643, -82.984093},
-    zoom: 18
-  };
-  var map=new google.maps.Map(document.getElementById("map"), mapO);
-}
-google.maps.event.addDomListener(window, 'load', initalize);
+// function initialize(){
+//   var mapO={
+//     center: {lat: 42.613643, -82.984093},
+//     zoom: 18
+//   };
+//   var map=new google.maps.Map(document.getElementById("map"), mapO);
+// }
+// google.maps.event.addDomListener(window, 'load', initalize);
+function initialize() {
+        var mapOptions = {
+          center: { lat: -34.397, lng: 150.644},
+          zoom: 8
+        };
+        var map = new google.maps.Map(document.getElementById('map'),
+            mapOptions);
+      }
+      google.maps.event.addDomListener(window, 'load', initialize);
