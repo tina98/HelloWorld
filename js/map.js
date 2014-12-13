1a389420e6944635b04b7248b5a2f939333aa666
tina98 <tinali3698@gmail.com>
2014-12-13 00:46:53 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,6 +1,8 @@
+var coord=new google.maps.LatLng(42.613643, -82.984093);
 function initialize() {
+  
   var mapO={
-    center: {lat: 42.613643, lng: -82.984093},
+    center: coord,
     zoom: 18
   };
   var map = new google.maps.Map(document.getElementById('map'), mapO);
