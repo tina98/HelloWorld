63ee6f92d0a8c6b907612d64b99b02f7506d773f
tina98 <tinali3698@gmail.com>
2014-12-13 00:40:41 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -1,9 +1,9 @@
-function initialize(){
+function initialize() {
   var mapO={
     center: {lat: 42.613643, lng: -82.984093},
     zoom: 18
   };
-  var map=new google.maps.Map(document.getElementById("map"), mapO);
+  var map = new google.maps.Map(document.getElementById('map'), mapO);
 }
 google.maps.event.addDomListener(window, 'load', initalize);
 // function initialize() {
