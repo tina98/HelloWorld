d3b9c854d2f3866ec2a30e9737adf4217a7223e2
tina98 <tinali3698@gmail.com>
2014-12-13 01:32:31 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -9,22 +9,22 @@ function initialize() {
 }
 function m1(){
   coord=new google.maps.LatLng(42.6142, -82.98402);
-  // document.getElementById("mapAdd").innerHTML("Instructional Resource Center (14201 Canal Rd., Sterling Heights MI, 48313)");
-  // document.getElementById("mapI").innerHTML("Transporation will not be provided to the IRC for Days 1-5");
+  document.getElementById("mapAdd").innerHTML="Instructional Resource Center (14201 Canal Rd., Sterling Heights MI, 48313)";
+  document.getElementById("mapI").innerHTML="Transporation will not be provided to the IRC for Days 1-5";
   
   initialize();
 }
 function m2(){
   coord=new google.maps.LatLng(42.2805, -83.743831);
-  // document.getElementById("mapAdd").innerHTML("Google Ann Arbor (201 South Division Street #500, Ann Arbor, MI 48104)");
-  document.getElementById("mapI").innerHTML("Transporation WILL be provided to Google Ann Arbor for Day 5");
+  document.getElementById("mapAdd").innerHTML="Google Ann Arbor (201 South Division Street #500, Ann Arbor, MI 48104)";
+  document.getElementById("mapI").innerHTML="Transporation WILL be provided to Google Ann Arbor for Day 5 (tentative)";
   
   initialize();
 }
 function m3(){
   coord=new google.maps.LatLng(42.2768, -83.741310);
-  // document.getElementById("mapAdd").innerHTML("University of Michigan (500 South State Street, Ann Arbor, MI 48109)");
-  // document.getElementById("mapI").innerHTML("Transporation WILL be provided back to the IRC for day 5 (tentative)");
+  document.getElementById("mapAdd").innerHTML="University of Michigan (500 South State Street, Ann Arbor, MI 48109)";
+  document.getElementById("mapI").innerHTML="Transporation WILL be provided back to the IRC for day 5 (tentative)";
   
   initialize();
 }
