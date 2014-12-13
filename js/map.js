de94a97985910dcc07883b7d42b7e4ab2958ce74
tina98 <tinali3698@gmail.com>
2014-12-13 01:07:23 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -7,4 +7,25 @@ function initialize() {
   };
   var map = new google.maps.Map(document.getElementById('map'), mapO);
 }
+function m1(){
+  var coord=new google.maps.LatLng(42.613643, -82.984093);
+  document.getElementById("mapAdd").innerHTML("Instructional Resource Center (14201 Canal Rd., Sterling Heights MI, 48313)");
+  document.getElementById("mapI").innerHTML("Transporation will not be provided to the IRC for Days 1-5");
+  
+  initialize();
+}
+function m2(){
+  var coord=new google.maps.LatLng(42.279966, -83.743831);
+  document.getElementById("mapAdd").innerHTML("Google Ann Arbor (201 South Division Street #500, Ann Arbor, MI 48104)");
+  document.getElementById("mapI").innerHTML("Transporation WILL be provided to Google Ann Arbor for Day 5 (tentative)");
+  
+  initialize();
+}
+function m3(){
+  var coord=new google.maps.LatLng(42.276238, -83.741310);
+  document.getElementById("mapAdd").innerHTML("University of Michigan (500 South State Street, Ann Arbor, MI 48109)");
+  document.getElementById("mapI").innerHTML("Transporation WILL be provided back to the IRC for day 5 (tentative)");
+  
+  initialize();
+}
 google.maps.event.addDomListener(window, 'load', initialize);
