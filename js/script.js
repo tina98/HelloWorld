0e187e3b7547e0a90e4b48d33f5250e8365533f8
tina98 <tinali3698@gmail.com>
2014-12-19 19:37:10 -0500

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,30 +1,30 @@
 $(document).ready(function(){
 
     var flagN=true;
-    $(window).scroll(function () {
-        if ($(window).scrollTop()>=($(window).height()-$("#navbar").height()*2)){
+    // $(window).scroll(function () {
+    //     if ($(window).scrollTop()>=($(window).height()-$("#navbar").height()*2)){
         
-            $('#navbar').css("top","0vw")
-              .css("position","fixed")
-              .css("background-color","rgba(20, 23, 23, 0.7)")
-              .css("width", "100%");
+    //         $('#navbar').css("top","0vw")
+    //           .css("position","fixed")
+    //           .css("background-color","rgba(20, 23, 23, 0.7)")
+    //           .css("width", "100%");
                 
-        } else {
-            $('#navbar')
-              .css("position","static")
-              .css("background-color","rgba(20, 23, 23, 1)");
-        }  
+    //     } else {
+    //         $('#navbar')
+    //           .css("position","static")
+    //           .css("background-color","rgba(20, 23, 23, 1)");
+    //     }  
       
-        if ($(window).scrollTop()>=($("#why").position().top)-2){
-            if (flagN){
-                numbers(18, "#i1");
-                numbers(12, "#i2");
-                numbers(79, "#i3");
-                flagN=false;
+    //     if ($(window).scrollTop()>=($("#why").position().top)-2){
+    //         if (flagN){
+    //             numbers(18, "#i1");
+    //             numbers(12, "#i2");
+    //             numbers(79, "#i3");
+    //             flagN=false;
                 
-            }
-        }
-    });
+    //         }
+    //     }
+    // });
     $("#homeIcon").click(function(){
        event.preventDefault();
        $("html, body").animate({scrollTop:0}, "slow");
