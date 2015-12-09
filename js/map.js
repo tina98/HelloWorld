ffdae2185a8cd46eeda97ba0103bb5456604a6a4
tina98 <tinali3698@gmail.com>
2015-12-08 20:17:29 -0500

    Update map.js

--- js/map.js
+++ js/map.js
@@ -3,10 +3,12 @@ var coord=new google.maps.LatLng(42.6142, -82.984093);
 var irc=new google.maps.LatLng(42.613643,-82.984093);
 var googleAA=new google.maps.LatLng(42.279966,-83.743831);
 var michigan=new google.maps.LatLng(42.276238, -83.741310);
+var microsoft=new google.maps.LatLng(42.459570, -83.239894);
 //exact lat-.00057
 var marker1;
 var marker2;
 var marker3;
+var marker4;
 
 
 function initialize() {
@@ -32,9 +34,15 @@ function initialize() {
     animation: google.maps.Animation.DROP,
     position: michigan
   });
+  marker4=new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: microsoft
+  });
   marker1.setAnimation(google.maps.Animation.BOUNCE);
   marker2.setAnimation(google.maps.Animation.BOUNCE);
   marker3.setAnimation(google.maps.Animation.BOUNCE);
+  marker4.setAnimation(google.maps.Animation.BOUNCE);
 }
 
 
@@ -56,6 +64,13 @@ function m3(){
   coord=new google.maps.LatLng(42.2768, -83.741310);
   document.getElementById("mapAdd").innerHTML="University of Michigan (500 South State Street, Ann Arbor, MI 48109)";
   document.getElementById("mapI").innerHTML="Transporation WILL be provided back to the IRC for day 5 (tentative)";
+  
+  initialize();
+}
+function m4(){
+  coord=new google.maps.LatLng(42.460132, -83.495695);
+  document.getElementById("mapAdd").innerHTML="Microsoft Technology Center (1000 Town Center Dr., Suite 1930, Southfield, MI 48075)";
+  document.getElementById("mapI").innerHTML="Transporation WILL be provided to and from the Microsoft Technology Center for Day 2 (tentative)";
   
   initialize();
 }
